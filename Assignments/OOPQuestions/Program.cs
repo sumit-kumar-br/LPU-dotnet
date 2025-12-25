32a30f0dca8fd7866910ec17b8bea9676cfd85f1
Sumit Kumar <roys930430@gmail.com>
2025-12-25 23:37:01 +0530

    Assignments 2

--- Assignments/OOPQuestions/Program.cs
+++ Assignments/OOPQuestions/Program.cs
@@ -10,12 +10,23 @@ public class Program
         double area = Math.PI * radius * radius;
         return Math.Round(area, 2, MidpointRounding.AwayFromZero);
     }
+    public static double ConvertFeetToCentimetres(int valueInFeet)
+    {
+        double valueInCm = valueInFeet * 30.48;
+        return Math.Round(valueInCm, 2, MidpointRounding.AwayFromZero);
+    }
     public static void Main(String[] args)
     {
-        double radius;
-        Console.WriteLine("Enter the radius");
-        radius = Double.Parse(Console.ReadLine());
-        Console.WriteLine($"Area of circle is {CalculateCircleArea(radius)}");
+        // double radius;
+        // Console.WriteLine("Enter the radius");
+        // radius = Double.Parse(Console.ReadLine());
+        // Console.WriteLine($"Area of circle is {CalculateCircleArea(radius)}");
+
+        int valueInFeet;
+        Console.WriteLine("Enter the value in feet");
+        valueInFeet = Int32.Parse(Console.ReadLine());
+        Console.WriteLine($"Value in centimetres is {ConvertFeetToCentimetres(valueInFeet)}");
+
 
     }
 }

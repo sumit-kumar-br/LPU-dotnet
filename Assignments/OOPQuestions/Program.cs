8e0c8a16f642554ef2e2bec8a7b767ee077dde4d
Sumit Kumar <roys930430@gmail.com>
2025-12-25 23:29:09 +0530

    A new console project for the assignment alongwith first question

---
+++ Assignments/OOPQuestions/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Program
+{
+    public static double CalculateCircleArea(double radius)
+    {
+        double area = Math.PI * radius * radius;
+        return Math.Round(area, 2, MidpointRounding.AwayFromZero);
+    }
+    public static void Main(String[] args)
+    {
+        double radius;
+        Console.WriteLine("Enter the radius");
+        radius = Double.Parse(Console.ReadLine());
+        Console.WriteLine($"Area of circle is {CalculateCircleArea(radius)}");
+
+    }
+}

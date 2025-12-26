41a66b9278cf082bde5d0c26144c7d22ea537242
Sumit Kumar <roys930430@gmail.com>
2025-12-26 22:00:05 +0530

    Classwork of 26 Dec

---
+++ Dec26a/Dec26a/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using Dec26b;
+
+namespace Dec26a
+{
+    class Monitor: Product
+    {
+        public Monitor(string str): base(str)
+        {
+            
+        }
+        public string Parts
+        {
+            get
+            {
+                return Ingridients;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+
+        }
+    }
+}

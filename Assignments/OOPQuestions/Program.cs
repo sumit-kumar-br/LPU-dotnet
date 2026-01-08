b3f1645c0aa453679ae9782b3d79116310699d0e
Sumit Kumar <roys930430@gmail.com>
2026-01-08 09:44:03 +0530

    Assignments to do

--- Assignments/OOPQuestions/Program.cs
+++ Assignments/OOPQuestions/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// some OOPS Questions
 
 using System;
 using System.Security.Cryptography.X509Certificates;

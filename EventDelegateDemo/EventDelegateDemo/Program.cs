233d1b5af2064977b3d31adc5a27e69560c735a8
Sumit Kumar <roys930430@gmail.com>
2026-01-09 23:53:00 +0530

    Program.cs

--- EventDelegateDemo/EventDelegateDemo/Program.cs
+++ EventDelegateDemo/EventDelegateDemo/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Threading;
+
+namespace EventDelegateDemo
+{
+    public delegate bool CreateRecord(Product p);
+    public delegate void Caller(string str);
+    delegate string strDelegate(string str);
+    public class Handler
+    {
+        public static string UpperCase(string s)
+        {
+            Thread.Sleep(5000);
+            return s.ToUpper();
+        }
+    }
+    class Program
+    {
+        public static void ShowMe(string str)
+        {
+            Console.WriteLine("ShowMe called");
+        }
+        public void GenerateBill(string str)
+        {
+            Console.WriteLine("GenerateBill called");
+        }
+        static void Main(string[] args)
+        {
+            //ProductRepo pRepo = new ProductRepo();
+            //CreateRecord AddProduct = new CreateRecord(pRepo.Add);
+            //AddProduct(new Product());
+
+            //Program p1 = new Program();
+            //Caller CallMe = new Caller(Program.ShowMe);
+            //CallMe += new Caller(p1.GenerateBill);
+            //CallMe("LPU");
+            //CallMe -= new Caller(p1.GenerateBill);
+            //CallMe("LPU");
+
+            strDelegate caller = new strDelegate(Handler.UpperCase);
+            //string res = caller("ThE quICk BrOWn FoX");
+            //Console.WriteLine(res);
+            //Console.WriteLine("Demo on Async delegate");
+
+            IAsyncResult result = caller.BeginInvoke("ThE quICk BrOWn FoX", null, null);
+            Console.WriteLine("Demo on Async delegate");
+            string returnValue = caller.EndInvoke(result);
+        }
+    }
+    
+}

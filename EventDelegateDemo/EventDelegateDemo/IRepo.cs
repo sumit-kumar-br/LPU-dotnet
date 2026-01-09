8c896350c98aa6555755a78dab218ae207d5acca
Sumit Kumar <roys930430@gmail.com>
2026-01-09 23:50:55 +0530

    demo on interfaces irepo

---
+++ EventDelegateDemo/EventDelegateDemo/IRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegateDemo
+{
+    public interface IRepo<T>
+    {
+        public bool Add(Product obj);
+        public Product SearchByID(int id);
+    }
+}

9ea9d854d3f6d7d52f8f4d9b088265954c64b0cb
Sumit Kumar <roys930430@gmail.com>
2026-01-09 23:52:23 +0530

    product entity details

---
+++ EventDelegateDemo/EventDelegateDemo/ProductRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegateDemo
+{
+    internal class ProductRepo : IRepo<Product>
+    {
+        public bool Add(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product SearchByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

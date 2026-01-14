0446d48e7a47bdc25ce11a4a78e8a8b840effcab
Sumit Kumar <roys930430@gmail.com>
2026-01-14 23:51:44 +0530

    Assembly and Secret key demo

---
+++ Jan14/AssemblyDemo/UILogic/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UILogic
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Property|AttributeTargets.Method,AllowMultiple = true )]
+    public class Doctor: Attribute
+    {
+        public string Name { get; set; }
+        public string CheckedOnDate { get; set; }
+    }
+}
---
+++ Jan14/AssemblyDemo/UILogic/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using ServiceLibrary;
+using System;
+
+namespace UILogic
+{
+    [Doctor(Name = "Ravi", CheckedOnDate = "12/02/2025")]
+    [Doctor(Name = "Neha", CheckedOnDate = "16/03/2025")]
+    [Doctor(Name = "Shashi", CheckedOnDate = "22/04/2025")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.Write("Enter 1st Number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter 2nd Number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            SomeLogic logic = new SomeLogic();
+
+            int numResult = logic.AddMe(num1, num2);
+            Console.WriteLine($"The Sum of {num1} and {num2} is {numResult}");
+
+            Doctor atr = (Doctor)Attribute.GetCustomAttribute(typeof(Program), typeof(Doctor));
+
+            Console.ReadLine();
+        }
+    }
+}

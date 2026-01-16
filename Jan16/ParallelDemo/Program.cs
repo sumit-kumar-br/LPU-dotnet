b17b2de97fbac93eb0cb6e4d312efa9bda457c83
Sumit Kumar <roys930430@gmail.com>
2026-01-16 23:41:00 +0530

    Demo on parallel programming

---
+++ Jan16/ParallelDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class MainClass {
+
+    public static void Main() {
+        int[] Numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // Single-threaded
+        Console.WriteLine("Normal foreach loop: ");
+        foreach (var number in Numbers) {
+            Console.WriteLine(longCalculation(number));
+        }
+        // This is the Parallel (Multi-threaded solution)
+        Console.WriteLine("Parallel foreach loop: ");
+        Parallel.ForEach(Numbers, number => {
+            Console.WriteLine(longCalculation(number));
+        });
+    }
+
+    private static int longCalculation(int number) {
+        Thread.Sleep(1000); // Sleep to simulate a long calculation
+        return number * number;
+    }
+}

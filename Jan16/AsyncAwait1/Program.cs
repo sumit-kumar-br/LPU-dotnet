4352d04864f1857a87485f0f7fc1b288321c7b23
Sumit Kumar <roys930430@gmail.com>
2026-01-16 23:36:54 +0530

    Demo on Async Await function

---
+++ Jan16/AsyncAwait1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+	
+	class Program  
+        {  
+            static void Main(string[] args)  
+            {  
+                Method1();  
+                Method2();  
+                Console.ReadLine();  
+            }  
+              
+            public static async Task Method1()  
+            {  
+                await Task.Run(() =>  
+                {  
+                    for (int i = 0; i < 100; i++)  
+                    {  
+                        Console.WriteLine("Method 1"+i);  
+                    }  
+                });             
+            }  
+      
+              
+    public static void Method2()  
+            {  
+                for (int i = 0; i < 25; i++)  
+                {  
+                    Console.WriteLine(" Method 2");  
+                }  
+            }  
+    }   

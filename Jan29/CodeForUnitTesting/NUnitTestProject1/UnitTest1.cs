0aae952c3e21350c5a5709fd759469e8d9e69762
Sumit Kumar <roys930430@gmail.com>
2026-01-29 23:51:11 +0530

    Demo Project for studying N Unit testing

---
+++ Jan29/CodeForUnitTesting/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+using CalculatorService;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        Calculator calcObj = null;
+        [SetUp]
+        public void Setup()
+        {
+            calcObj = new Calculator();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [TearDown]
+        public void Reset()
+        {
+            calcObj = null;
+        }
+    }
+}

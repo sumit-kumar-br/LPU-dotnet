4bddfac5db9d02167bc109ebcb81ac3705c7eb70
Sumit Kumar <roys930430@gmail.com>
2026-01-31 23:51:36 +0530

    Bike repo class for Bike On Rent project

---
+++ Practice/BikeOnRent_App/BikeOnRent_App/BikeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeOnRent_App
+{
+        public class BikeRepository : IBikeRepository
+        {
+            private readonly SortedDictionary<int, Bike> _store = new();
+
+            public void AddBike(Bike bike)
+            {
+                _store.Add(_store.Count + 1, bike);
+            }
+
+            public SortedDictionary<int, Bike> GetAllBikes()
+            {
+                return _store;
+            }
+        }   
+}

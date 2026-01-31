70dc2dd3699948d20952a7387b66347af4a895f6
Sumit Kumar <roys930430@gmail.com>
2026-01-31 23:51:13 +0530

    Bike entity class for Bike On Rent project

---
+++ Practice/BikeOnRent_App/BikeOnRent_App/Bike.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeOnRent_App
+{
+    public class Bike
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int PricePerDay { get; set; }
+    }
+}

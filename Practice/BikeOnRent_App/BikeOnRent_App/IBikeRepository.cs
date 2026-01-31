239f20a9ed71717551f4aaee1821f8f63edc0bd2
Sumit Kumar <roys930430@gmail.com>
2026-01-31 23:51:57 +0530

    interface for Bike On Rent project

---
+++ Practice/BikeOnRent_App/BikeOnRent_App/IBikeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeOnRent_App
+{
+    public interface IBikeRepository
+    {
+        void AddBike(Bike bike);
+        SortedDictionary<int, Bike> GetAllBikes();
+    }
+}

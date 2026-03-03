b3e39364184aacf1ad901205a6228a212f0db9db
Sumit Kumar <roys930430@gmail.com>
2026-03-03 09:40:35 +0530

    Flight Search Engine

---
+++ WebApplication1/WebApplication1/Data/DatabaseHelper.cs
@@ -0,0 +1,116 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<string>> GetSourcesAsync()
+        {
+            var list = new List<string>();
+
+            using SqlConnection con = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_GetSources", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            await con.OpenAsync();
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                list.Add(reader["Source"].ToString());
+            }
+
+            return list;
+        }
+
+        public async Task<List<string>> GetDestinationsAsync()
+        {
+            var list = new List<string>();
+
+            using SqlConnection con = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_GetDestinations", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            await con.OpenAsync();
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                list.Add(reader["Destination"].ToString());
+            }
+
+            return list;
+        }
+
+        public async Task<List<FlightResult>> SearchFlightsAsync(string source, string destination, int persons)
+        {
+            var list = new List<FlightResult>();
+
+            using SqlConnection con = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_SearchFlights", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Source", source);
+            cmd.Parameters.AddWithValue("@Destination", destination);
+            cmd.Parameters.AddWithValue("@Persons", persons);
+
+            await con.OpenAsync();
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                list.Add(new FlightResult
+                {
+                    FlightId = Convert.ToInt32(reader["FlightId"]),
+                    FlightName = reader["FlightName"].ToString(),
+                    FlightType = reader["FlightType"].ToString(),
+                    Source = reader["Source"].ToString(),
+                    Destination = reader["Destination"].ToString(),
+                    TotalCost = Convert.ToDecimal(reader["TotalCost"])
+                });
+            }
+
+            return list;
+        }
+
+        public async Task<List<FlightHotelResult>> SearchFlightsWithHotelsAsync(string source, string destination, int persons)
+        {
+            var list = new List<FlightHotelResult>();
+
+            using SqlConnection con = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_SearchFlightsWithHotels", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Source", source);
+            cmd.Parameters.AddWithValue("@Destination", destination);
+            cmd.Parameters.AddWithValue("@Persons", persons);
+
+            await con.OpenAsync();
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                list.Add(new FlightHotelResult
+                {
+                    FlightId = Convert.ToInt32(reader["FlightId"]),
+                    FlightName = reader["FlightName"].ToString(),
+                    Source = reader["Source"].ToString(),
+                    Destination = reader["Destination"].ToString(),
+                    HotelName = reader["HotelName"].ToString(),
+                    TotalCost = Convert.ToDecimal(reader["TotalCost"])
+                });
+            }
+
+            return list;
+        }
+    }
+}

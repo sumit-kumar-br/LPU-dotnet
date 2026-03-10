daf7299a6b3e1f9f372aeb5d1832461e460eabef
Sumit Kumar <roys930430@gmail.com>
2026-03-10 10:39:03 +0530

    Demo for CRUD in web api

---
+++ Practice/EmployeeAdminPanel/EmployeeAdminPanel/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeAdminPanel.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPanel.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)  
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
